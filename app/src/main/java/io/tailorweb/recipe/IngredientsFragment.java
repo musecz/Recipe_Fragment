2b4c6308b40d93ce7e5a34ec259c4649052b4385
musecz <musecz@wanadoo.fr>
2017-02-03 09:58:23 +0100

    Initial commit

---
+++ app/src/main/java/io/tailorweb/recipe/IngredientsFragment.java
@@ -0,0 +1,20 @@
+package io.tailorweb.recipe;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.zip.Inflater;
+
+public class IngredientsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_ingredients, container, false);
+        return view;
+    }
+}
