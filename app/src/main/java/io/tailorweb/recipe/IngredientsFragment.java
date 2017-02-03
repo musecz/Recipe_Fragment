bab1b75bdaf4120324e643070e92bd9eb497d1d1
musecz <musecz@wanadoo.fr>
2017-02-03 10:29:18 +0100

    adding checkboxes

--- app/src/main/java/io/tailorweb/recipe/IngredientsFragment.java
+++ app/src/main/java/io/tailorweb/recipe/IngredientsFragment.java
@@ -7,14 +7,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
-import java.util.zip.Inflater;
 
 public class IngredientsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        int index = getArguments().getInt(ViewPagerFragment.KEY_RECIPE_INDEX);
         View view = inflater.inflate(R.layout.fragment_ingredients, container, false);
+        LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.ingredientsLayout);
+        String[] ingredients = Recipes.ingredients[index].split("`");
+        setUpCheckBoxes(ingredients, linearLayout);
         return view;
+    }
+
+    private void setUpCheckBoxes(String[] ingredients, ViewGroup container) {
+        for (String ingredient : ingredients) {
+            CheckBox checkBox = new CheckBox(getContext());
+            checkBox.setPadding(8, 16, 8, 16);
+            checkBox.setTextSize(20f);
+            checkBox.setText(ingredient);
+            container.addView(checkBox);
+        }
     }
 }
--- app/src/main/java/io/tailorweb/recipe/ViewPagerFragment.java
+++ app/src/main/java/io/tailorweb/recipe/ViewPagerFragment.java
@@ -21,6 +21,9 @@ public class ViewPagerFragment extends Fragment {
         getActivity().setTitle(Recipes.names[index]);
         View view = inflater.inflate(R.layout.fragment_viewpager, container, false);
         final IngredientsFragment ingredientsFragment = new IngredientsFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(KEY_RECIPE_INDEX, index);
+        ingredientsFragment.setArguments(bundle);
         final DirectionsFragment directionsFragment = new DirectionsFragment();
         ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewPager);
         viewPager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
